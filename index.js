27e7fc2d72a5065f8a509ffaf0b3484cb8e8d471
Giselle <gisellesol18@gmail.com>
2024-12-16 12:49:49 -0300

    modo oscuro y claro mas input de cambiar tex

--- index.js
+++ index.js
@@ -4,12 +4,22 @@
  const btnCerrarPanel = document.querySelector("#btnCerrarPanel");
  const btnTex = document.querySelector("#btnTex");
  const btnImg = document.querySelector("#btnImg");
+ const btnToogleTheme = document.querySelector("#btnToogleTheme");
 
  const panelImg = document.querySelector("#panelImg");
 
+ //Input imgs
  const inputUrl = document.querySelector("#inputUrl");
+ 
+ //Input texts
+ const inputTopText =  document.querySelector("#inputTopText");
+
+  const darkTheme = document.querySelector ("body.darkTheme");
+  const lightTheme = document.querySelector ("body.lightTheme");
 
  const contenedorImg = document.querySelector(".contenedorImg")
+ const topText = document.querySelector("#topText");
+
 
 // Eventos - panel de imagen y texto//
  btnCerrarPanel.addEventListener("click", (e) => {
@@ -32,6 +42,19 @@
    panelText.style.display = "block";
  });
 
- inputUrl.addEventListener("input",(e) => {
-   console.log (e.target.value)
- })
+
+btnToogleTheme.addEventListener("click", (e) => {
+  if(btnToogleTheme.textContent == "Modo Claro"){
+    document.body.classList.remove("lightTheme");
+    document.body.classList.add("darkTheme");
+    btnToogleTheme.textContent = "Modo Oscuro";
+  }else{
+    document.body.classList.remove("darkTheme");
+    document.body.classList.add("lightTheme");
+    btnToogleTheme.textContent = "Modo Claro";
+  }
+})
+
+inputTopText.addEventListener("input",(e)=>{
+  topText.textContent = e.target.value;
+})
